af983396b614e6f5a267a8ae1da18f1eb80e299c
timothyrusso <russotimothy@live.it>
2022-02-23 08:18:58 +0100

    feat: cards routes and controllers

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const usersRouter = require('./routes/users')
+const cardsRouter = require('./routes/cards')
 
 const { PORT = 3000 } = process.env;
 
-app.use('/', usersRouter)
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
 
 app.listen(PORT, () => {
   console.log('The server is started');
