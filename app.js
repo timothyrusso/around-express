35f74ac814c5728a1e73959a22af9a962ac3e024
timothyrusso <russotimothy@live.it>
2022-03-12 14:55:19 +0100

    feat: connection with the database

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const helmet = require('helmet');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const mongoose = require('mongoose');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false
+});
 
 app.use(helmet());
 app.use('/', usersRouter);
