2ca18a88d27e85ded3376d48ab5cba93536959a5
timothyrusso <russotimothy@live.it>
2022-03-13 11:31:32 +0100

    feat: users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,10 @@
-const path = require('path');
-const getDataFromFile = require('../helpers/files');
+const User = require('../models/user');
 
-const dataPath = path.join(__dirname, '..', 'data', 'users.json');
-
-const getUsers = (req, res) => getDataFromFile(dataPath)
+const getUsers = (req, res) => User.find({})
   .then((users) => res.status(200).send(users))
   .catch((err) => res.status(500).send(err));
 
-const getProfile = (req, res) => getDataFromFile(dataPath)
+const getProfile = (req, res) => User.findById(req.params.id)
   .then((users) => users.find((user) => user._id === req.params.id))
   .then((user) => {
     if (!user) {
@@ -20,4 +17,11 @@ const getProfile = (req, res) => getDataFromFile(dataPath)
   })
   .catch((err) => res.status(500).send(err));
 
-module.exports = { getUsers, getProfile };
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((users) => res.status(201).send({ data: users }))
+    .catch((err) => res.status(500).send(err));
+}
+
+module.exports = { getUsers, getProfile, createUser };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { getUsers, getProfile } = require('../controllers/users');
+const { getUsers, getProfile, createUser } = require('../controllers/users');
 
 router.get('/users', getUsers);
 
-router.get('/users/:id', getProfile);
+router.get('/users/:userId', getProfile);
+
+router.post('/users', createUser);
 
 module.exports = router;
