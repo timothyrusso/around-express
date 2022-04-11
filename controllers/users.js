0973856f812aa69acfa6ccbc2ea3dfc7afe67a90
timothyrusso <russotimothy@live.it>
2022-04-11 07:48:56 +0200

    feat: default fields for the user model

--- controllers/users.js
+++ controllers/users.js
@@ -28,8 +28,8 @@ const getProfile = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const { name, about, avatar, email, password } = req.body;
+  User.create({ name, about, avatar, email, password })
     .then((user) => res.status(RESOURCE_CREATED).send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
--- models/user.js
+++ models/user.js
@@ -4,18 +4,21 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    default: 'Jacques Cousteau',
     required: true,
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
+    default: 'Explorer',
     required: true,
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/avatar_1604080799.jpg',
     required: [true, 'url required'],
     validate: {
       validator: (v) => validator.isURL(v),
