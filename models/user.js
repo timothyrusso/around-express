d81ef898ea8d9ffbbb9c50389d244089c117c2d7
timothyrusso <russotimothy@live.it>
2022-04-13 06:40:43 +0200

    feat: Stopping the API from returning the password hash

--- models/user.js
+++ models/user.js
@@ -37,12 +37,12 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 8
+    select: false,
   },
 });
 
 userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Incorrect email or password'));
